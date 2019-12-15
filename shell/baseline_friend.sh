47dd1601e8a06d386d38681b0512a60c7504fc92
Soo95 <plmko32@gmail.com>
2019-12-15 16:37:02 +0900

    Add more test cases

---
+++ shell/baseline_friend.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo NV_GPU=0 nvidia-docker run \
+ -it \
+ --rm \
+ -v "/home/hslee1/ee543:/tmp/ee543" \
+ -v "/home/Alexandrite/hslee:/workspace" \
+ --name "debug" \
+ --shm-size "32G" \
+ hslee:pytorch-v1.3 \
+ /bin/bash -c 'cd /tmp/ee543 &&
+  python -m baseline.Train --config_path=/tmp/ee543/config/baseline_friend.yaml'
